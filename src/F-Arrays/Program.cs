b65828598622158c466153b09cc67a7b501a7563
Jvmachado01 <joaommachado89@outlook.com>
2024-05-29 18:18:40 -0300

    Arrays

---
+++ src/F-Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //int[] numeros; // declaração 
+
+        //quantidade = new int[5]; // inicialização com tamanho fixo
+
+        int[] idades = new int[] {20, 19, 18, 23, 25}; // incializando com elementos
+    
+    
+        Console.WriteLine(idades[1]); // 19        
+        idades[0] = 30;
+        Console.WriteLine(idades[0]); // 30
+
+        for (int i = 0; i < idades.Length; i++) {
+            Console.WriteLine(idades[i]);
+        }
+
+        foreach (int idade in idades) {
+            Console.WriteLine(idade);
+        }
+    }
+}
